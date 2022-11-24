e46a7b5678d77d317fda2c9e3d8613a0a6ed6242
Samarendra Dash <samarendra109@gmail.com>
2022-11-24 00:38:46 -0500

    q2 en first attempt

--- q2_en.pl
+++ q2_en.pl
@@ -71,14 +71,14 @@ speaks ---> (v,
 
 % Phrase structure rules (incomplete)
 npforall rule
-    (np, logic:@forall(X, @apply(X_sem, [X]), (logic, P))) ===>
+    (np, logic:@forall((qvar, X), @apply(X_sem, [X]), (logic, P))) ===>
     cat> (q, logic:forall),
-    sem_head> (X, n, logic:f:X_sem).
+    sem_head> (n, logic:f:X_sem).
 
 npexists rule
-    (np, logic:@exists(X, @apply(X_sem, [X]), (logic, P))) ===>
+    (np, logic:@exists((qvar, X), @apply(X_sem, [X]), (logic, P))) ===>
     cat> (q, logic:exists),
-    sem_head> (X, n, logic:f:X_sem).
+    sem_head> (n, logic:f:X_sem).
 
 vp rule
     (vp, logic:LF) ===>
