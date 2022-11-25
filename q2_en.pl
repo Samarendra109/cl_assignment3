3d1f5f65e9f5e7c0212df35d5f5249ac33d37cc0
Samarendra Dash <samarendra109@gmail.com>
2022-11-25 15:27:48 -0500

    Q2 new attempt after tutorial

--- q2_en.pl
+++ q2_en.pl
@@ -75,10 +75,10 @@ speaks ---> (v,
 
 % Phrase structure rules (incomplete)
 np rule
-    (np, NP_logic) ===>
+    (np, @apply(Q_logic, [N_logic])) ===>
     cat> (q, logic: Q_logic),
-    sem_head> (n, logic: N_logic),
-    goal> beta_normalize(@apply(Q_logic, [N_logic]), NP_logic).
+    sem_head> (n, logic: N_logic).
+    % goal> beta_normalize(@apply(Q_logic, [N_logic]), NP_logic).
 
 vp rule
     (vp) ===>
