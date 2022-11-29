650159c0e40971de64e2860f4ba9ed0bf3ce65ab
Samarendra Dash <samarendra109@gmail.com>
2022-11-29 00:08:45 -0500

    Working for part c

--- q2_en2.pl
+++ q2_en2.pl
@@ -98,8 +98,8 @@ vp rule
 
 s rule
     (s, logic: S_logic, qstore: S_qstore) ===>
-    cat> (np, logic:NP_logic, qstore: e_list),
-    sem_head> (vp, logic:VP_logic, qstore: (ne_list, VP_qstore)),
+    cat> (np, logic:NP_logic),
+    sem_head> (vp, logic:VP_logic, qstore: VP_qstore),
     goal> apply_normalize_and_retrieve(
         NP_logic, VP_logic, VP_qstore, S_logic, S_qstore
     ).
