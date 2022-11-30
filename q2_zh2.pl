bbe544d9eb76e90949025a0563537391f7ec5dfc
Samarendra Dash <samarendra109@gmail.com>
2022-11-30 15:21:23 -0500

    Subcat list with chinese ambiguity

--- q2_zh2.pl
+++ q2_zh2.pl
@@ -163,12 +163,12 @@ is_ambiguous((np, agr:NP1_agr), (np, agr:NP2_agr)) if
     bn_quant(NP2_agr, exists).
 
 apply_normalize_and_retrieve(NP1, [NP2], LogicFunc, LogicArg, QStore, NewLogic, NewQStore) if
-    is_ambiguous(NP1, NP2),
+    % is_ambiguous(NP1, NP2),
     beta_normalize(@apply(LogicFunc, [LogicArg]), Norm_logic),
     retrieve(QStore, Norm_logic, NewQStore, NewLogic).
 
 apply_normalize_and_retrieve(NP1, [NP2], LogicFunc, LogicArg, QStore, Norm_logic, QStore) if
-    not(is_ambiguous(NP1, NP2)),
+    % not(is_ambiguous(NP1, NP2)),
     is_empty(QStore),
     beta_normalize(@apply(LogicFunc, [LogicArg]), Norm_logic).
 
