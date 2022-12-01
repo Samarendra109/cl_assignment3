aba9adbf350609f1ac9fcfd8d41f6d3d169ae2d9
Samarendra Dash <samarendra109@gmail.com>
2022-12-01 16:11:30 -0500

    debugging ambiguity check

--- q2_zh5.pl
+++ q2_zh5.pl
@@ -185,9 +185,8 @@ apply_normalize_and_retrieve(NP1, [NP2], LogicFunc, LogicArg, QStore, Norm_logic
 is_not_gap(none) if true.
 is_gap(np) if true.
 
-check_gap_and_normalize(V_logic, (np, logic: NP_logic, gap:Gap), VP_logic) if
-    is_not_gap(Gap),
-    beta_normalize(@apply(V_logic, [NP_logic]), VP_logic).
+check_gap_and_normalize(V_logic, (np, logic: NP_logic, gap:Gap), @apply(V_logic, [NP_logic])) if
+    is_not_gap(Gap).
 
 check_gap_and_normalize(V_logic, (np, logic: NP_logic, gap:Gap), V_logic) if
     is_gap(Gap).
